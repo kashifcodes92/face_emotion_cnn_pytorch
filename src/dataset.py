ce5e02493fe3be3390d0f2eec456c55637554fc9
Kashif Ali <kashif.codes93@gmail.com>
2026-01-15 16:00:37 +0500

    dataset.py update

--- src/dataset.py
+++ src/dataset.py
@@ -4,20 +4,20 @@ import os
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
-def get_dataloader(
-        data_dir, batch_size=64,
-        shuffle=True,
-        num_workers=2
-        ):
+
+def get_dataloaders(
+    data_root="/kaggle/input/fer2013",
+    batch_size=64,
+    num_workers=2
+):
+    """
+    Returns train_loader, test_loader, and class names.
     """
 
-    Returns trainloader,testloader, and classnames.
-    """
+    train_dir = os.path.join(data_root, "train")
+    test_dir = os.path.join(data_root, "test")
 
-    train_dir = os.path.join(data_dir, "train")
-    test_dir = os.path.join(data_dir, "test")
-
-    train_transform = transforms.Compose([
+    train_transforms = transforms.Compose([
         transforms.Grayscale(num_output_channels=1),
         transforms.Resize((48, 48)),
         transforms.RandomHorizontalFlip(p=0.5),
@@ -25,34 +25,35 @@ def get_dataloader(
         transforms.Normalize(mean=[0.5], std=[0.5])
     ])
 
-    test_transform = transforms.Compose([
+    test_transforms = transforms.Compose([
         transforms.Grayscale(num_output_channels=1),
-        transforms.Resize((48,48)),
+        transforms.Resize((48, 48)),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.5,], std=[0.5])
+        transforms.Normalize(mean=[0.5], std=[0.5])
     ])
 
     train_dataset = datasets.ImageFolder(
         root=train_dir,
-        transform=train_transform
-        )
-    
+        transform=train_transforms
+    )
+
     test_dataset = datasets.ImageFolder(
         root=test_dir,
-        transform=test_transform
-        )
+        transform=test_transforms
+    )
 
     train_loader = DataLoader(
         train_dataset,
         batch_size=batch_size,
         shuffle=True,
         num_workers=num_workers
-        )
+    )
+
     test_loader = DataLoader(
         test_dataset,
         batch_size=batch_size,
         shuffle=False,
         num_workers=num_workers
-        )
+    )
 
     return train_loader, test_loader, train_dataset.classes
